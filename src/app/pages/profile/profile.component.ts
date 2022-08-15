62b4eed90966e40e53ec692d081268e79158e152
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-08-15 23:51:48 +0600

    error TS2531: Object is possibly 'null'.

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user = null;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.user = this.authService.getUser();
   }
-
 }
